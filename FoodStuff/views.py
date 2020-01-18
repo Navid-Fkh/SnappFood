21fb69f605f6ed29da7aa38a69d4d216d2c23df2
Navid.Far <mojtaba.farokhi99@gmail.com>
2020-01-18 14:23:25 +0330

    start apps

---
+++ FoodStuff/views.py
@@ -0,0 +1,16 @@
+from rest_framework import status
+from rest_framework.response import Response
+from django.db import connection
+
+
+# Create your views here.
+
+def create_tables(request):
+    with connection.cursor() as cursor:
+        try:
+            query = ""
+            cursor.execute(query)
+            result = cursor.fetchall()
+        except Exception as ex:
+            error = str(ex)
+    return Response(error, status=status.HTTP_400_BAD_REQUEST)
